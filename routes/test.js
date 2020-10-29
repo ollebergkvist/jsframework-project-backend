0d1e059c11df0ec597e8b0fc013ccbb1fb486920
Olle Bergkvist <ollebergkvist@gmail.com>
2020-10-29 12:25:10 -0600

    Revised test route

--- routes/test.js
+++ routes/test.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   const data = {
     data: {
-      msg: `Server working`,
+      msg: `Server up and running`,
     },
   };
 
